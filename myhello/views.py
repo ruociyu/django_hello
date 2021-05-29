b662b290ac5a34299269a9a2e825fcc0d1a32432
ruociyu <m6bji4h6@gmail.com>
2021-05-29 17:27:56 +0800

    -

--- myhello/views.py
+++ myhello/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def myIndex(request):
-    my_name = request.GET.get('name' , "CGU")
-    # my_name = request.POST.get('name' , "CGU")
+   #  my_name = request.GET.get('name' , "CGU")
+    my_name = request.POST.get('name' , "CGU")
     return HttpResponse("Hello "+ my_name)
 
